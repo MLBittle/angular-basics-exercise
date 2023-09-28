90a18956b4be8cc3167343dcb4359416fea026ff
Mary Linda Bittle <140030405+MLBittle@users.noreply.github.com>
2023-09-28 15:53:30 -0500

    angular-class-exercises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+@Component({
+  selector: 'app-name',
+  templateUrl: './name.component.html',
+  styleUrls: ['./name.component.css']
+})
+
+
+
 export class AppComponent {
   title = 'angular-basics-exercise';
 }
+
+export class NameComponent {
+  name = 'Mary Linda';
+  }
+
